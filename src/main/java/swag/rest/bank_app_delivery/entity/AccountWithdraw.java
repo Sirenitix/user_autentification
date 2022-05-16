25f07bd782e0374976f5f76aa01b93175ffbf3dd
suleevn <suleevn@gmail.com>
2022-05-17 00:45:31 +0600

    small fix

--- src/main/java/swag/rest/bank_app_delivery/entity/AccountWithdraw.java
+++ src/main/java/swag/rest/bank_app_delivery/entity/AccountWithdraw.java
@@ -3,7 +3,7 @@ package swag.rest.bank_app_delivery.entity;
 import org.springframework.stereotype.Component;
 
 
-public class AccountWithdraw extends Account {
+public abstract class AccountWithdraw extends Account {
 
     public AccountWithdraw(AccountType accountType, long id, String clientID, long bankID, double balance, boolean withdrawAllowed) {
         super(accountType, id, clientID, bankID, balance, withdrawAllowed);
