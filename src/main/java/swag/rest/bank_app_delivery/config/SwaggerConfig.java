4194d49466420b359e32e8a704e799c1095f46bf
NX <suleevn@gmail.com>
2022-05-24 16:30:15 +0300

    upd:swagger added

---
+++ src/main/java/swag/rest/bank_app_delivery/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package swag.rest.bank_app_delivery.config;
+
+import com.google.common.base.Predicate;
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Bank API",
+                "API for bank operations",
+                "1.0",
+                "http://www.termsofservice.org",
+                new Contact("API owner", "http://www.ownersite.com", "owner@rmailer.org"),
+                "api_license",
+                "http://www.license.edu.org",
+                new ArrayList<>()
+        );
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
---
+++ src/main/java/swag/rest/bank_app_delivery/controller/AccountRestController.java
@@ -0,0 +1,25 @@
+package swag.rest.bank_app_delivery.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import swag.rest.bank_app_delivery.entity.Account;
+import swag.rest.bank_app_delivery.service.internal.DBService;
+
+import java.util.List;
+
+@Api
+@RestController("/")
+public class AccountRestController  {
+
+    @Autowired
+    DBService dbService;
+
+
+    @GetMapping("/accounts")
+    public List<Account> getProducts(){
+        return dbService.getClientAccounts();
+    }
+
+}
